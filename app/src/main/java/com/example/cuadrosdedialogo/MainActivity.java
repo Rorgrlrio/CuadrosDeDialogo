78694bdde655b42263395738845424165e84290a
Rogelio Reynaga <roger.rs.04.07.02@gmail.com>
2022-10-31 09:34:22 -0600

    Agregando vista Calendario, agregando funciones

--- app/src/main/java/com/example/cuadrosdedialogo/MainActivity.java
+++ app/src/main/java/com/example/cuadrosdedialogo/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -19,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Instancias
     private EditText txtFecha, txtHora;
-    private int dia, mes, hora, minutos;
+    private int dia, mes, anio, hora, minutos;
     private Calendar calendario;
 
     @Override
@@ -85,4 +92,44 @@ public class MainActivity extends AppCompatActivity {
         });
         alertDialog.show();
     } //mostrarCuadroPersonalizado
-}
+
+    public void mostrarCuadroFecha(View view){
+        dia = calendario.get(Calendar.DAY_OF_MONTH);
+        mes = calendario.get(Calendar.MONTH);
+        anio = calendario.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, R.style.Theme_CuadrosDeDialogo, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                txtFecha.setText(year + "/" + month + "/" + dayOfMonth);
+            }
+        }, anio, mes, dia);
+
+        //Cambiar el fondo de los dias del mes
+        datePickerDialog.getWindow().setBackgroundDrawable(
+                new ColorDrawable(Color.GREEN)
+        );
+        //Mostrar el calendario
+        datePickerDialog.show();
+
+    } //INCOMPLETO
+
+    public void mostrarCuadroHora(View view) {
+        hora = calendario.get(Calendar.HOUR_OF_DAY);
+        minutos = calendario.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, R.style.Theme_CuadrosDeDialogo, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                txtHora.setText(hourOfDay + ":" + minute);
+            }
+        }, hora, minutos, true);
+        timePickerDialog.show();
+    }//mostrarCuadroHora
+
+    public void mostrarCalendario(View view){
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }//MostrarCalendario
+
+}//classS
