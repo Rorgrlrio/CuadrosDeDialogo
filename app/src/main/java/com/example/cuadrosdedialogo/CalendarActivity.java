81e2aecad5b05003ff6d60d7c17adee98d9cbbb9
Rogelio Reynaga <roger.rs.04.07.02@gmail.com>
2022-10-31 09:39:30 -0600

    Añadiendo config a CalendarActivity

--- app/src/main/java/com/example/cuadrosdedialogo/CalendarActivity.java
+++ app/src/main/java/com/example/cuadrosdedialogo/CalendarActivity.java
@@ -1,14 +1,34 @@
 package com.example.cuadrosdedialogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    //Instancia
+    CalendarView cal;
+    private int dia, mes, anio, hora, minutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        cal = findViewById(R.id.cvCalendario);
+
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String fecha = year + "/" + month + "/" + dayOfMonth;
+                Toast.makeText(CalendarActivity.this, fecha, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
